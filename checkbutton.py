7ac3f404350a5f0c0e322812d585a762f87bb97b
althoren95 <karlodanda@gmail.com>
2025-08-19 19:11:25 +0200

    chk_btn

--- checkbutton.py
+++ checkbutton.py
@@ -1,17 +1,36 @@
 import tkinter as tk
 
-def show_check_status():
-    print(var.get())
+def show_status():
+    """Update the label based on the checkbox state."""
+    if var1.get() and var2.get():
+        text='Both options selected!'
+    elif var1.get():
+        text='Only Option 1 selected!'
+    elif var2.get():
+        text='Only Option 2 selected!'
+    else:   
+        text='No options selected!'
+
+    lab.config(text=text)
+    send_btn.config(state='disabled')
 
 root = tk.Tk()
 root.title('My GUI Application')
 root.geometry('400x300')
-root.minsize(200, 150)
 
-var = tk.BooleanVar()
-chk = tk.Checkbutton(root, text='Check me!', variable=var, font=('Arial', 16))                     
-chk.pack(pady=20)
+var1 = tk.BooleanVar()
+var2 = tk.BooleanVar()
 
-tk.Button(root, text='Show Status', command=show_check_status).pack(pady=10)
+chk1 = tk.Checkbutton(root, text='Option 1', variable=var1)
+chk1.pack(pady=20)
+
+chk2 = tk.Checkbutton(root, text='Option 2', variable=var2)
+chk2.pack(pady=20)
+
+send_btn = tk.Button(root, text='Send', command=show_status)
+send_btn.pack(pady=20)
+
+lab = tk.Label(root, text='Check the box to agree.')
+lab.pack(pady=20)
 
 root.mainloop()
